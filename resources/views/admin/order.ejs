6148445dfa60fd2f7fe5180a98cbfa7f03cd7333
Harshks2406 <harshsingh.hks@gmail.com>
2022-06-17 10:33:14 +0530

    orders completed

---
+++ resources/views/admin/order.ejs
@@ -0,0 +1,19 @@
+<section class="order light-section">
+    <div class="container mx-auto p-12">
+        <h1 class="font-bold-text-lg-mb-4">All orders</h1>
+        <table class="w-full table-auto bg-white">
+            <thead>
+                <tr>
+                    <th class="px-4 py-2 text-left">Orders</th>
+                    <th class="px-4 py-2 text-left">Customer</th>
+                    <th class="px-4 py-2 text-left">Phone</th>
+                    <th class="px-4 py-2 text-left">Status</th>
+                    <th class="px-4 py-2 text-left">Placed at</th>
+                </tr>
+            </thead>
+            <tbody>
+                
+            </tbody>
+        </table>
+    </div>
+</section>
--- routes/web.js
+++ routes/web.js
@@ -2,6 +2,8 @@ const authController = require('../app/http/controllers/authController')
 const cartController = require('../app/http/controllers/customer/cartController')
 const orderController = require('../app/http/controllers/customer/orderController')
 const homeController = require('../app/http/controllers/homeController')
+const adminOrderController = require('../app/http/controllers/admin/orderController')
+const auth = require('../app/http/middlewares/auth')
 const guest = require('../app/http/middlewares/guest')
 
 function initRoutes(app){
@@ -19,10 +21,13 @@ function initRoutes(app){
 
     app.post("/logout",authController().logout)
 
-    app.post("/orders",orderController().store)
+    app.post("/orders",auth,orderController().store)
 
     //customer routes
-    app.get("/customer/orders",orderController().index)
+    app.get("/customer/orders",auth,orderController().index)
+
+    //admin routes
+    app.get("/admin/orders",auth,adminOrderController().index)
 }
 
 module.exports = initRoutes
